97621b635ceb603bc3b5449712bf30084be5e71c
linixiis <160885821+linixiis@users.noreply.github.com>
2024-05-25 18:10:56 +0300

    Add files via upload

---
+++ emotions/script.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: true,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
